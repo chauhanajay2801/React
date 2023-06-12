97791d1f75029112fed0073c007413efa03ea1ec
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-06-12 14:37:09 +0530

    Day1

---
+++ Day1/App.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement("h1",
+// { id:"heading"},
+// "Hey There");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    React.createElement("h1", {}, "I am heading")
+  )
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
