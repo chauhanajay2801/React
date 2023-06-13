1161ddbc2d76ebbd1c9d1b2f04a866ce15f1791a
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-06-13 22:25:05 +0530

    Igniting2

--- Day2/App.js
+++ Day2/App.js
@@ -1,16 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1",
 // { id:"heading"},
 // "Hey There");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    React.createElement("h1", {}, "I am heading")
+const parent = React.createElement("div",{ id: "parent" },
+  React.createElement("div",{ id: "child" },[
+    React.createElement("h1", {}, "I am heading"),
+    React.createElement("h2", {}, "I am heading2")
+  ]
   )
 );
 
