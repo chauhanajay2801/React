710a1566d3229b928850d0f2ad789b7b54ffacb9
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-06-17 21:34:23 +0530

    Day6

---
+++ Day6/src/components/Shimmer.js
@@ -0,0 +1,20 @@
+
+const Shimmer = () => {
+  return (
+    <div className="shimer-container">
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+        <div className="shimer-cards"></div>
+    </div>
+  )
+}
+
+export default Shimmer
