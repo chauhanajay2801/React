243bf9d0fdb9fd99078ae801af5c062cff38000e
Ajay Chauhan <Chauhanajay2801@gmail.com>
2023-06-18 21:42:56 +0530

    child routes added

--- Day7/src/App.js
+++ Day7/src/App.js
@@ -5,25 +5,29 @@ import Body from "./components/Body";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error"
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider,Outlet} from "react-router-dom";
 
 const AppLayout = () => {
   return(
     <div className="App">
      <Header/>
-     <Body/>
+     <Outlet/>
     </div>
   )
 }
 
 const appRouter = createBrowserRouter([
   {
-    path:"/",element:<AppLayout/>,errorElement : <Error/>
-  },{
-    path:'/about',element:<About/>
-  },
-  {
-    path:"/contact", element:<Contact/>
+    path:"/",element:<AppLayout/>,
+    children:[
+      {
+        path:'/about',element:<About/>
+      },
+      {
+        path:"/contact", element:<Contact/>
+      }
+    ]
+    ,errorElement : <Error/>
   }
 ])
 const root = ReactDOM.createRoot(document.getElementById("root"));
